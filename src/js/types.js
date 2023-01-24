b6935fc16ced0372be11d980a47a85cc039e6888
Alisa <alisakovalhome@mail.ru>
2023-01-24 21:38:26 +0300

    Add more in types

--- src/js/types.js
+++ src/js/types.js
@@ -1,4 +1,31 @@
+let button = document.querySelector('#buttonCheck')
+
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.tap-target');
     var instances = M.TapTarget.init(elems, options);
-  });
+});
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.scrollspy');
+    var instances = M.ScrollSpy.init(elems, options);
+});
+
+
+button.addEventListener('click', () => {
+    let input = document.querySelector('#inputCheck').value
+    let checkResult = document.querySelector('#checkResult')
+    if (Number(input)) {
+        checkResult.innerHTML = 'Number'
+    } else if (typeof Object & !typeof null) {
+        if (typeof Array) {
+            checkResult.innerHTML = 'Array'
+        } else {
+            checkResult.innerHTML = 'Object'
+        }
+        
+    } else if (typeof null) {
+        checkResult.innerHTML = 'null'
+    } 
+
+
+})
