14512822dde9917da5986e0e6042005824513485
Alisa <alisakovalhome@mail.ru>
2023-01-25 21:54:45 +0300

    Add strings & null in cards

--- src/js/arrays.js
+++ src/js/arrays.js
@@ -1,59 +1,59 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.collapsible');
-    var instances = M.Collapsible.init(elems);
-});
+// document.addEventListener('DOMContentLoaded', function () {
+//     var elems = document.querySelectorAll('.collapsible');
+//     var instances = M.Collapsible.init(elems);
+// });
 
-let done = document.querySelector('#done');
-done.addEventListener('click', (e) => {
-    e.preventDefault();
+// let done = document.querySelector('#done');
+// done.addEventListener('click', (e) => {
+//     e.preventDefault();
 
-    let numbers = [];
-    let inputs = document.querySelectorAll(".number");
+//     let numbers = [];
+//     let inputs = document.querySelectorAll(".number");
 
-    for (let input of inputs) {
-        numbers.push(Number(input.value));
-    }
+//     for (let input of inputs) {
+//         numbers.push(Number(input.value));
+//     }
 
-    let n = numbers.length;
-    for (let i = 0; i < n - 1; i++) {
-        let min = i;
-        for (let j = i + 1; j < n; j++) {
-            if (numbers[j] < numbers[min]) min = j;
-        }
-        let t = numbers[min];
-        numbers[min] = numbers[i];
-        numbers[i] = t;
-    }
+//     let n = numbers.length;
+//     for (let i = 0; i < n - 1; i++) {
+//         let min = i;
+//         for (let j = i + 1; j < n; j++) {
+//             if (numbers[j] < numbers[min]) min = j;
+//         }
+//         let t = numbers[min];
+//         numbers[min] = numbers[i];
+//         numbers[i] = t;
+//     }
 
-    let sum = numbers.reduce(function (sum, elem) {
-        return sum + elem;
-    }, 0);
+//     let sum = numbers.reduce(function (sum, elem) {
+//         return sum + elem;
+//     }, 0);
 
-    document.querySelector("#out").innerHTML = numbers;
-    document.querySelector('#sum').innerHTML = sum;
+//     document.querySelector("#out").innerHTML = numbers;
+//     document.querySelector('#sum').innerHTML = sum;
 
-})
+// })
 
-let add = document.querySelector('#hide');
-add.addEventListener('click', (e) => {
-    e.preventDefault();
+// let add = document.querySelector('#hide');
+// add.addEventListener('click', (e) => {
+//     e.preventDefault();
 
-    let x = 0;
-    let str = '<div><input class="number" type="text" /></div> <div id="input' + (x + 1) + '"></div>';
-    document.getElementById('input' + x).innerHTML = str;
-    x++;
+//     let x = 0;
+//     let str = '<div><input class="number" type="text" /></div> <div id="input' + (x + 1) + '"></div>';
+//     document.getElementById('input' + x).innerHTML = str;
+//     x++;
 
-})
+// })
 
-let reset = document.querySelector('#reset');
-reset.addEventListener('click', (e) => {
-    e.preventDefault();
+// let reset = document.querySelector('#reset');
+// reset.addEventListener('click', (e) => {
+//     e.preventDefault();
 
-    let inputs = document.querySelectorAll(".number");
-    for (let i2 = 0; i2 < inputs.length; i2++) {
-        inputs[i2].value = '';
-    };
-    document.querySelector("#out").innerHTML = "";
-    document.querySelector("#sum").innerHTML = "";
+//     let inputs = document.querySelectorAll(".number");
+//     for (let i2 = 0; i2 < inputs.length; i2++) {
+//         inputs[i2].value = '';
+//     };
+//     document.querySelector("#out").innerHTML = "";
+//     document.querySelector("#sum").innerHTML = "";
 
-})
+// })
--- src/js/types.js
+++ src/js/types.js
@@ -2,30 +2,31 @@ let button = document.querySelector('#buttonCheck')
 
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.tap-target');
-    var instances = M.TapTarget.init(elems, options);
+    var instances = M.TapTarget.init(elems);
 });
 
   document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.scrollspy');
-    var instances = M.ScrollSpy.init(elems, options);
+    var instances = M.ScrollSpy.init(elems);
 });
 
 
-button.addEventListener('click', () => {
-    let input = document.querySelector('#inputCheck').value
-    let checkResult = document.querySelector('#checkResult')
-    if (Number(input)) {
-        checkResult.innerHTML = 'Number'
-    } else if (typeof Object & !typeof null) {
-        if (typeof Array) {
-            checkResult.innerHTML = 'Array'
-        } else {
-            checkResult.innerHTML = 'Object'
-        }
+
+// button.addEventListener('click', () => {
+//     let input = document.querySelector('#inputCheck').value
+//     let checkResult = document.querySelector('#checkResult')
+//     if (Number(input)) {
+//         checkResult.innerHTML = 'Number'
+//     } else if (typeof Object & !typeof null) {
+//         if (typeof Array) {
+//             checkResult.innerHTML = 'Array'
+//         } else {
+//             checkResult.innerHTML = 'Object'
+//         }
         
-    } else if (typeof null) {
-        checkResult.innerHTML = 'null'
-    } 
+//     } else if (typeof null) {
+//         checkResult.innerHTML = 'null'
+//     } 
 
 
-})
+// })
