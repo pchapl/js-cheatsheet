b0b842e738a1972c04607feb11a2b086b853c56b
YuliaLugovaya <yulialugovayaphoto@gmail.com>
2023-01-26 21:16:59 +0300

    added some code for arrays

--- src/js/arrays.js
+++ src/js/arrays.js
@@ -1,3 +1,16 @@
+/*document.addEventListener('DOMContentLoaded', () => {
+    hljs.highlightAll();
+    document.querySelector('code.codep span.hljs-string').innerHTML = `<input id="arrays" type="text">`;
+});*/
+
+/*document.addEventListener('DOMContentLoaded', (event) => {
+    document.querySelectorAll('pre code code.code').forEach((el) => {
+        hljs.highlightElement(el);
+        document.querySelectorAll('pre code code.code span.hljs-string').innerHTML = `<input id="arrays-simple__data" type="text">`;
+    });
+});*/
+
+
 //materialize
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -8,14 +21,65 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
-//button Done
+//global
 
-let done = document.querySelector('#arrays-simple__done');
-done?.addEventListener('click', (e) => {
-    e.preventDefault();
+let globalOld = document.querySelector('.arrays__old');
+let globalDelete = document.querySelector(".arrays__delete");
+let globalResult = document.querySelector('.arrays__result');
+let globalInput = document.querySelector(".arrays__input");
 
+
+//addEventListener
+
+let buttonsDone = document.querySelectorAll('.arrays__button_done');
+buttonsDone.forEach(function(done) {
+    done?.addEventListener('click', (e) => {
+        e.preventDefault();
+        donePop();
+    })
+})
+
+let buttonsAdd = document.querySelectorAll('.arrays__button_add');
+buttonsAdd.forEach(function(add) {
+    add?.addEventListener('click', (e) => {
+        e.preventDefault();
+        addInputPop();
+    })
+})
+
+let buttonsReset = document.querySelectorAll('.arrays__button_reset');
+buttonsReset.forEach(function(reset) {
+    reset?.addEventListener('click', (e) => {
+        e.preventDefault();
+        resetPop();
+    })
+})
+
+let buttonsCheck = document.querySelectorAll('.arrays__check');
+buttonsCheck.forEach(function(check) {
+    check?.addEventListener('click', (e) => {
+        e.preventDefault();
+        checkPop();
+        checkPush();
+    })
+})
+
+
+//add
+
+function addInputPop() {
+    let parent = document.querySelector('.arrays__input');
+    let input = document.createElement('div');
+    input.innerHTML = `<input id="arrays__data" type="text">`;
+    parent.appendChild(input);
+}
+
+
+//done
+
+function donePop() {
     let elements = [];
-    let data = document.querySelectorAll("#arrays-simple__data");
+    let data = document.querySelectorAll("#arrays__data");
 
     for (let i = 0; i < data.length; i++) {
         let d = data[i].value;
@@ -35,11 +99,7 @@ done?.addEventListener('click', (e) => {
         }
     }
 
-    console.log(old)
-    console.log(elements)
-
     let del = elements.pop();
-    console.log(elements)
 
     let result = [];
     for (let i = 0; i < elements.length; i++) {
@@ -50,54 +110,37 @@ done?.addEventListener('click', (e) => {
         }
     }
 
-    document.querySelector('#arrays-simple__old').innerHTML = 'let array = [' + old + '];';
-    document.querySelector("#arrays-simple__delete").innerHTML = del;
-    document.querySelector('#arrays-simple__result').innerHTML = 'let array = [' + result + '];';
-})
-
-
-//button Add
-
-function addInput() {
-    let parent = document.querySelector('#arrays__input');
-    let input = document.createElement('div');
-    input.innerHTML = `<input id="arrays-simple__data" type="text">`;
-    parent.appendChild(input);
-
+    globalOld.innerHTML = 'let array = [' + old + '];';
+    globalDelete.innerHTML = del;
+    globalResult.innerHTML = 'let array = [' + result + '];';
 }
 
-let add = document.querySelector('#arrays-simple__add');
-add?.addEventListener('click', (e) => {
-    e.preventDefault();
-    addInput();
-})
 
+//reset
 
-//button Reset
-
-let reset = document.querySelector('#arrays-simple__reset');
-reset?.addEventListener('click', (e) => {
-    e.preventDefault();
-
-    let data = document.querySelectorAll("#arrays-simple__data");
+function resetPop() {
+    let data = document.querySelectorAll("#arrays__data");
     for (let i = 0; i < data.length; i++) {
         data[i].value = '';
     };
 
-    document.querySelector("#arrays__input").innerHTML = "";
-    document.querySelector("#arrays-simple__result").innerHTML = "";
-    document.querySelector("#arrays-simple__delete").innerHTML = "";
-    document.querySelector('#arrays-simple__old').innerHTML = "";
-})
+    globalInput.innerHTML = "";
+    globalResult.innerHTML = "";
+    globalDelete.innerHTML = "";
+    globalOld.innerHTML = "";
+}
 
 
-//button Check
+//check
 
-let check = document.querySelector('.arrays__check');
-check?.addEventListener('click', (e) => {
-    e.preventDefault();
-
+function checkPop() {
     let fruits = ["Вишня", "Абрикос", "Черника"];
     alert(fruits.pop());
     alert(fruits);
-})
+}
+
+function checkPush() {
+    let fruits = ["Вишня", "Абрикос"];
+    fruits.push("Черника");
+    alert(fruits);
+}
