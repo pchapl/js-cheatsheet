50c84e0df2f0108b198dcc851e9eed6eb0d88b8a
YuliaLugovaya <yulialugovayaphoto@gmail.com>
2023-01-26 05:19:49 +0300

    added some code for pop in arrays

--- src/js/arrays.js
+++ src/js/arrays.js
@@ -13,54 +13,48 @@ done?.addEventListener('click', (e) => {
     let data = document.querySelectorAll(".arrays-simple__data");
 
     for (let i = 0; i < data.length; i++) {
-        elements.push(Number(data[i].value));
+        if (isNaN(data[i].value)) {
+            elements.push(data[i].value);
+        } else {
+            elements.push(Number(data[i].value));
+        }
     }
 
-    let sum = elements.reduce(function (sum, elem) {
-        return sum + elem;
-    }, 0);
+    let old = [];
+    for (let i = 0; i < data.length; i++) {
+        if (isNaN(data[i].value)) {
+            old.push(' ' + '"' + data[i].value + '"');
+        } else {
+            old.push(' ' + data[i].value);
+        }
+    }
 
-    /*let a = 0;
-    let code;
+    console.log(old)
+    console.log(elements)
+
+    let del = elements.pop();
+    console.log(elements)
+
+    let result = [];
     for (let i = 0; i < elements.length; i++) {
-
-        let arr = [];
-
-        //elements.push(Number(data[i].value));
-        code = `<div class="arrays-code">
-        <pre>
-        <code class="js">
-        let array = [${elements[i] + a}]; 
-        alert(array.pop()); 
-        console.log(array);
-        </code>
-        </pre>
-        </div>`;
-        elements[i]++;
-        a++;
+        if (isNaN(elements[i])) {
+            result.push(' ' + '"' + elements[i] + '"');
+        } else {
+            result.push(' ' + elements[i]);
+        }
     }
-    console.log(code);*/
-
-    let code = `<div class="arrays-code">
-    <pre>
-    <code class="js">
-    let array = [${elements}]; 
-    alert(array.pop()); 
-    console.log(array);
-    </code>
-    </pre>
-    </div>`;
 
 
-    document.querySelector("#arrays-simple__out").innerHTML = code;
-    document.querySelector('#arrays-simple__result').innerHTML = sum;
+    document.querySelector('#arrays-simple__old').innerHTML = 'let array = [' + old + '];';
+    document.querySelector("#arrays-simple__delete").innerHTML = del;
+    document.querySelector('#arrays-simple__result').innerHTML = 'let array = [' + result + '];';
 
 })
 
 let item = 0;
 function addInput() {
-    let string = '<div><input class="arrays-simple__data" type="text" /></div> <div id="arrays-simple__input' + (item + 1) + '"></div>';
-    document.getElementById('arrays-simple__input' + item).innerHTML = string;
+    let string = '<div><input class="arrays-simple__data" type="text" /></div> <div class="arrays-simple__input' + (item + 1) + '"></div>';
+    document.querySelector('.arrays-simple__input' + item).innerHTML = string;
     item++;
 }
 
@@ -78,7 +72,18 @@ reset?.addEventListener('click', (e) => {
     for (let i = 0; i < data.length; i++) {
         data[i].value = '';
     };
-    document.querySelector("#arrays-simple__out").innerHTML = "";
+    document.querySelector(".arrays-simple__input0").innerHTML = "[]";
     document.querySelector("#arrays-simple__result").innerHTML = "";
+    document.querySelector("#arrays-simple__delete").innerHTML = "";
+    document.querySelector('#arrays-simple__old').innerHTML = "";
 
+})
+
+let check = document.querySelector('.arrays__check');
+check?.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    let fruits = ["Вишня", "Абрикос", "Черника"];
+    alert(fruits.pop());
+    alert(fruits);
 })
