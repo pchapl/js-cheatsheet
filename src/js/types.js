9abf226f05f49dc56397378c5175512162cceef1
Alisa <alisakovalhome@mail.ru>
2023-01-27 17:27:36 +0300

    Add symol card

--- src/js/types.js
+++ src/js/types.js
@@ -11,17 +11,34 @@ document.addEventListener('DOMContentLoaded', function() {
 }); 
 
 
+const object1 = {};
+const a = Symbol('a');
+const b = Symbol.for('b');
+
+object1[a] = 'localSymbol';
+object1[b] = 'globalSymbol';
+
+const objectSymbols = Object.getOwnPropertySymbols(object1);
+
+console.log(objectSymbols);
+// Expected output: 2
+
+
+
 button?.addEventListener('click', () => {
-    let input = document.querySelector('#inputCheck').value
+    let input = document.querySelector('#inputCheck')
     let checkResult = document.querySelector('#checkResult')
-    if (Number(input)) {
+    
+    if (Number(input.value)) {
         checkResult.innerHTML = 'Number'
-    } else if (typeof Object & !typeof null) {
+    } else if (typeof input.value === Object) {
         checkResult.innerHTML = 'Объект'
         
-    } else if (typeof null) {
+    } else if (input.value = 'null' && input.value != 'undefined') {
         checkResult.innerHTML = 'null'
-    } 
+    } else if (input.value = 'undefined') {
+        checkResult.innerHTML = 'undefined'
+    }
 
 
 })
