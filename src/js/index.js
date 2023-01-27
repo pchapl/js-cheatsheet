e570c392db4dbed08a3afe26ac419c6f3ea767bb
Sirius Black <mariia.beliaev@gmail.com>
2023-01-27 19:29:49 +0300

    a workable version of index page

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,4 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     let options = {
-//         dropdownOptions: {
-//             alignment: 'right',
-//             hover: true
-//         }
-//     }
-//     let elems = document.querySelectorAll('.dropdown-trigger');
-//     let instances = M.Dropdown.init(elems, options);
-// });
-
 document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.dropdown-trigger');
-    var instances = M.Dropdown.init(elems, {
-        alignment: 'right',
-//             hover: true
-    });
+    let elems = document.querySelectorAll('.dropdown-trigger');
+    let instances = M.Dropdown.init(elems);
 });
