a07268026c8f190cfd69af85997c57c3b80cb207
GRLPWR1 <110125337+GRLPWR1@users.noreply.github.com>
2023-02-02 01:45:20 +0300

    brunched out, added html, js, and scss files

--- src/js/index.js
+++ src/js/index.js
@@ -8,3 +8,4 @@ document?.addEventListener('DOMContentLoaded', function () {
     let elems = document.querySelectorAll('.dropdown-trigger');
     let instances = M.Dropdown.init(elems, options);
 });
+import './js/this.js';
