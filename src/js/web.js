1099b30174f37de6476451835ac97bbe8aa995af
YuliaLugovaya <yulialugovayaphoto@gmail.com>
2023-02-02 21:25:22 +0300

    added content for web

--- src/js/web.js
+++ src/js/web.js
@@ -22,6 +22,7 @@ buttonSuccess?.addEventListener('click', (e) => {
     async function success() {
         let response = await fetch("https://api.giphy.com/v1/gifs/translate?api_key=C0ZP0xR1klDe6ID8tIORjR17tQTEUjYx&s=cat");
         let data = await response.json();
+        console.log(data)
         let headersObject = [...response.headers];
 
         let values = Object.values(headersObject);
@@ -37,19 +38,19 @@ buttonSuccess?.addEventListener('click', (e) => {
 
         try {
             if(response.status === 200){
-                document.querySelector(".web-gif-success").innerHTML += `<img src="${data.data.images.fixed_height_small.url}" alt = "">`;
-                document.querySelector(".web-status-success").innerHTML = `Status Code: ${response.status}`;
+                document.querySelector(".web-gif-success").innerHTML += `<img src="${data.data.images.fixed_height.url}" alt = "">`;
+                document.querySelector(".web-status-success").innerHTML = `Status Code: <i class="tiny material-icons web-iconcode-green">fiber_manual_record</i>${response.status}`;
                 document.querySelector(".web-headers-success").innerHTML = "Headers:";
                 for (let i = 0; i < headersStr.length; i++) {
                     document.querySelector(".web-header-success").innerHTML += `<div class="web-header">${headersStr[i]}</div>`;
                 }           
             } else {
-                throw new Error ("Ошибка сервера. Проверьте адрес запроса и заголовки.");
+                throw new Error ("Проверьте корректность запроса.");
             }          
         } catch (error) {
             console.log(error.message);
             document.querySelector(".web-gif-success").innerHTML = error.message;
-            document.querySelector(".web-status-success").innerHTML = `Status Code: ${response.status}`;
+            document.querySelector(".web-status-success").innerHTML = `Status Code: <i class="tiny material-icons web-iconcode-red">fiber_manual_record</i>${response.status}`;
             document.querySelector(".web-headers-success").innerHTML = "Headers:";
             for (let i = 0; i < headersStr.length; i++) {
                 document.querySelector(".web-header-success").innerHTML += `<div class="web-header">${headersStr[i]}</div>`;
@@ -82,20 +83,20 @@ buttonUnsuccess?.addEventListener('click', (e) => {
 
         try {
             if(response.status === 200){
-                document.querySelector(".web-gif-unsuccess").innerHTML += `<img src="${data.data.images.fixed_height_small.url}" alt = "">`;
-                document.querySelector(".web-status-unsuccess").innerHTML = `Status Code: ${response.status}`;
+                document.querySelector(".web-gif-unsuccess").innerHTML += `<img src="${data.data.images.fixed_height.url}" alt = "">`;
+                document.querySelector(".web-status-unsuccess").innerHTML = `Status Code: <i class="tiny material-icons web-iconcode-green">fiber_manual_record</i>${response.status}`;
                 document.querySelector(".web-headers-unsuccess").innerHTML = "Headers:";
                 for (let i = 0; i < headersStr.length; i++) {
                     document.querySelector(".web-header-unsuccess").innerHTML += `<div class="web-header">${headersStr[i]}</div>`;
                 }     
                 
             } else {
-                throw new Error ("Ошибка сервера. Проверьте адрес запроса и заголовки.");
+                throw new Error ("Проверьте корректность запроса.");
             }           
         } catch (error) {
             console.log(error.message);
             document.querySelector(".web-gif-unsuccess").innerHTML = error;
-            document.querySelector(".web-status-unsuccess").innerHTML = `Status Code: ${response.status}`;
+            document.querySelector(".web-status-unsuccess").innerHTML = `Status Code: <i class="tiny material-icons web-iconcode-red">fiber_manual_record</i>${response.status}`;
             document.querySelector(".web-headers-unsuccess").innerHTML = "Headers:";
             for (let i = 0; i < headersStr.length; i++) {
                 document.querySelector(".web-header-unsuccess").innerHTML += `<div class="web-header">${headersStr[i]}</div>`;
