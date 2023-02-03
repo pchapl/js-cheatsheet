8e068d3803c5d68929232bff1f90b714182dc851
Alisa <alisakovalhome@mail.ru>
2023-02-03 03:25:11 +0300

    Add 2tasks in prototypes

--- src/js/prototypes.js
+++ src/js/prototypes.js
@@ -1,12 +1,33 @@
-const person = new Object({
-    name: 'Nicolas',
-    age: 33,
+let button1 = document.querySelector('#chooseAnswer')
+let button2 = document.querySelector('#checkAnswer')
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems);
+});
+
+button1?.addEventListener('click', () => {
+    let result = document.querySelector('#result')
+    let radioBtn = document.querySelectorAll('input[name = "answer"]')
+    console.log(radioBtn);
+
+    if (radioBtn[0].checked) {
+        result.innerHTML = 'Это правильный ответ'
+    } else {
+        result.innerHTML = 'Это неправильный ответ'
+    }
 })
 
+button2?.addEventListener('click', () => {
+    let result = document.querySelector('#resultTask2')
+    let input = document.querySelector('.tasks__input2')
+
+    if (input.value === 'Array.prototype') {
+        result.innerHTML = 'Это правильный ответ'
+    } else {
+        result.innerHTML = 'Это неправильный ответ'
+    }
 
 
-console.log(person);
+})
 
-const a = 'string'
-console.log(a.length);
-console.log(a.__proto__);
