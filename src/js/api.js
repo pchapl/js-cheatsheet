f7f7f198e85ef30c0b0fb6205ac95efccbf039d3
Sirus <mariia.beliaev@gmail.com>
2023-02-03 15:29:23 +0300

    added about us page

--- src/js/api.js
+++ src/js/api.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
+document?.addEventListener('DOMContentLoaded', function () {
     let elems = document.querySelectorAll('.collapsible');
     let instances = M.Collapsible.init(elems, {
         accordion: false
