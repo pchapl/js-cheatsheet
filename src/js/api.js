49c8806d768c010057e4d91eb16aa499a9d9e397
Sirus <mariia.beliaev@gmail.com>
2023-02-04 17:24:06 +0300

    fixed api js

--- src/js/api.js
+++ src/js/api.js
@@ -21,32 +21,55 @@ document?.addEventListener("DOMContentLoaded", function (event) {
         .catch((error) => console.log(error));
 });
 
-document?.addEventListener("DOMContentLoaded", function (event) {
+document?.addEventListener("click", function (event) {
+    if (!event.target.matches("#yesno-button")) return;
+
     fetch("https://yesno.wtf/api")
         .then((response) => response.json())
-        .then((data) => {
-            const answer = document.querySelector(".answer");
-            answer.innerHTML = data.answer;
-            const img = document.querySelector(".img");
-            img.src = data.image;
-        })
-        .catch((error) => console.log(error));
+        .then((data) => getAnswer(data))
+        .catch(() => catchError());
 });
 
-document?.addEventListener("DOMContentLoaded", function (event) {
+function getAnswer(data) {
+    const answer = document.getElementById("answer");
+    const gif = document.getElementById("gif");
+    const error = document.getElementById("yesno-error");
+
+    error.innerHTML = "";
+    answer.innerHTML = data.answer;
+    gif.src = data.image;
+}
+
+function catchError() {
+    const error = document.getElementById("yesno-error");
+    error.innerHTML = "Советы закончились :(";
+}
+
+document.addEventListener("click", function (event) {
+    if (!event.target.matches("#activity-button")) return;
+
     fetch("https://www.boredapi.com/api/activity/")
         .then((response) => response.json())
-        .then((data) => {
-            const activity = document.querySelector(".activity");
-            activity.innerHTML = data.activity;
-        })
-        .catch((error) => console.log(error));
+        .then((data) => getActivity(data))
+        .catch(() => catchError());
 });
+
+function getActivity(data) {
+    const activity = document.getElementById("activity");
+    const error = document.getElementById("activity-error");
+
+    error.innerHTML = "";
+    activity.innerHTML = data.activity;
+}
+
+function catchError() {
+    error.innerHTML = "Ой, придумайте сами себе занятие!";
+}
 
 const container = document.getElementById("pokemon");
 
 document?.addEventListener("click", function (event) {
-    if (!event.target.matches("#button")) return;
+    if (!event.target.matches("#pokemon-button")) return;
 
     const pokemonNumber = getRandomInt(1, 893);
 
@@ -71,7 +94,7 @@ function renderPokemon(data) {
 }
 
 function renderError() {
-    container.innerHTML = "Whoops, something went wrong. Please try again!";
+    container.innerHTML = "Что-то пошло не так, покемон сбежал.";
 }
 
 function getRandomInt(min, max) {
